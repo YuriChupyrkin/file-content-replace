c9f9f422f5d5e990162bd0f9cda6dc96dec23938
YuriChupyrkin <Yuri.Chupyrkin@gmail.com>
2019-05-28 10:23:48 +0300

    Get all components with 'events' property

--- src/file-updater.js
+++ src/file-updater.js
@@ -17,8 +17,10 @@ async function getFiles (dir, extension) {
     if (stat.isDirectory()) {
       let foundFiles = await getFiles(filePath, extension); //recurse
       matchFiles = matchFiles.concat(foundFiles);
-    } else if (path.extname(filePath) == extension) {
-      matchFiles.push(filePath);
+    } else if (path.extname(filePath) == extension &&
+      filePath.includes('js/components/') &&
+      !files[i].includes('_spec')) {
+        matchFiles.push(filePath);
     };
   }
 
@@ -68,10 +70,56 @@ async function update (config, files) {
     console.log(`Updated! Read count: ${readFilesCount}; Write count: ${writeFilesCount}`);
   });
 };
+const getComponentsWithEvents = async function (files) {
+  let promises = [];
+  let readFilesCount = 0;
+  let components = [];
+
+  files.forEach((filePath) => {
+    let promise = new Promise((resolve, reject) => {
+
+      fs.readFile(filePath, 'utf8', (err, fileContent) => {
+        readFilesCount++
+
+        if (err) {
+          console.error(err);
+          throw new Error(`cannot read file: ${filePath}`);
+        }
+
+        let match = fileContent.match('events: {');
+
+        if (match && match.length > 0) {
+          let fileName = path.basename(filePath).split('.js')[0];
+          components.push(fileName);
+        }
+
+        resolve();
+      });
+    });
+
+    promises.push(promise);
+  });
+
+  await Promise.all(promises).then(() => {
+    console.log(`getComponentsWithEvents DONE! Read count: ${readFilesCount}`);
+  });
+
+  return components;
+}
 
 const updateFiles = async function (config) {
   const files = await getFiles(config.dir, config.extension);
-  await update(config, files);
+  const components = await getComponentsWithEvents(files);
+
+  console.log('Components with Events:');
+  components.forEach((componentFileName) => {
+    console.log(componentFileName);
+  });
+
+  console.log(`components found: ${components.length}`);
+  console.warn('===========================================');
+  console.warn('===========================================');
+  console.warn('===========================================');
 };
 
 module.exports = updateFiles;
